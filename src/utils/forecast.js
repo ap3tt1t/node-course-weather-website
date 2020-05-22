305455394f159799b5c52246163d84122e25da12
ap3tt1t <pettit.andrew@gmail.com>
2020-05-22 13:33:44 +0200

    Printed out more weather data from API

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,8 +1,11 @@
 const request = require('postman-request')
 require('dotenv').config()
 const weather_api_key = process.env.WEATHER_API_KEY
+
+
 const forecast = (lat, long, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=${weather_api_key}&query=${lat},${long}`
+    //return callback(url, url)
     request({
         url,
         json: true
@@ -12,7 +15,7 @@ const forecast = (lat, long, callback) => {
         } else if (body.success === false){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees but feels like ${body.current.feelslike} degrees. The humidity is ${body.current.humidity}%.`)
         }
     })
 }
